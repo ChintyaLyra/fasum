bfa13075fff560cae957372e79cf115df5c3216b
Chintya Lyra Hartanto <chintyalyra@mhs.mdp.ac.id>
2024-05-17 22:46:08 +0700

    Home Screen
    Tampilkan data hasil posting pada HomeScreen dalam bentuk ListView yang berisi CardView. Pada CardView terdapa foto, nama pengguna, waktu posting, dan deskripsi.

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,85 +1,164 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/home_screen.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AddPostScreen extends StatefulWidget {
-  const AddPostScreen({super.key});
-
   @override
-  State<AddPostScreen> createState() => _AddPostScreenState();
+  _AddPostScreenState createState() => _AddPostScreenState();
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
-  TextEditingController _controllerDesc = TextEditingController();
-  //XFile ? _selectedImage;
+  final _controllerDesc = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  XFile? _image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Post'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: GestureDetector(
-              onTap: (){
-                _pickImageFromCamera();
-              },
-              child: Image.asset(
-                'assets/images/add_photo.png',
-                width: 250,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controllerDesc,
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Description',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
             onPressed: () {
-              //_productService.addProductItem(_controllerKode.text, _controllerNama.text,  _controllerDesc.text);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const HomeScreen()));
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blueAccent,
-              foregroundColor: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              textStyle: const TextStyle(
-                fontSize: 18,
-                //fontWeight: FontWeight.bold
-              ),
-            ),
-            child: const Text('Post'),
+            icon: const Icon(Icons.arrow_back),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  await _showImageSourceDialog();
+                },
+                child: Container(
+                  child: _image != null
+                      ? Image.file(File(_image!.path))
+                      : Image.asset('assets/images/add_photo.png'),
+                  ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _controllerDesc,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Description',
+
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    //fontWeight: FontWeight.bold
+                  ),
+                ),
+                onPressed: () async {
+                  if (_image == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Please select an image')),
+                    );
+                    return;
+                  }
+
+                  Reference referenceRoot = FirebaseStorage.instance.ref();
+                  Reference referenceDirImages = referenceRoot.child("images");
+                  Reference referenceImagesToUpload =
+                  referenceDirImages.child(_image!.path.split("/").last);
+
+                  try {
+                    final uploadTask =
+                    await referenceImagesToUpload.putFile(File(_image!.path));
+                    final downloadUrl = await uploadTask.ref.getDownloadURL();
+
+                    // Add Firebase Cloud Firestore functionality here
+                    final CollectionReference posts =
+                    FirebaseFirestore.instance.collection('add_post');
+                    await posts.add({
+                      'desc': _controllerDesc.text,
+                      'url': downloadUrl,
+                      'time': Timestamp.now(),
+                      'email': _auth.currentUser?.email,
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Post Added! :D')),
+                    );
+
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const HomeScreen()));
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Error when uploading the image: $e')),
+                    );
+                  }
+                },
+                child: Text('Post'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  Future _pickImageFromCamera() async {
-    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
-  //   if(returnedImage==null) return;
-  //   setState(() {
-  //     _selectedImage = XFile(returnedImage!.path);
-  //   });
+  Future<void> _showImageSourceDialog() async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Choose Image Source'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Text('Open Camera'),
+              onTap: () async {
+                Navigator.of(context).pop();
+                final pickedFile =
+                await _picker.pickImage(source: ImageSource.camera);
+                if (pickedFile != null) {
+                  setState(() {
+                    _image = pickedFile;
+                  });
+                }
+              },
+            ),
+            ListTile(
+              title: Text('Pick from Gallery'),
+              onTap: () async {
+                Navigator.of(context).pop();
+                final pickedFile =
+                await _picker.pickImage(source: ImageSource.gallery);
+                if (pickedFile != null) {
+                  setState(() {
+                    _image = pickedFile;
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controllerDesc.dispose();
+    super.dispose();
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/add_post_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => SignInScreen()));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,16 +37,59 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('You have logged In'),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('add_post').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot documentSnapshot =
+                  snapshot.data!.docs[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(
+                        'Description: ${documentSnapshot['desc']}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      leading: Container(
+                        width: 70,
+                        height: 70,
+                        child: Image.network(
+                          documentSnapshot['url'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'User: ${_auth.currentUser?.email == documentSnapshot['email'] ? _auth.currentUser?.email ?? 'Unknown' : documentSnapshot['user_email']}',
+                          ),
+                          Text(DateFormat.yMMMd().add_jm().format(
+                            documentSnapshot['time'].toDate(),
+                          )),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const AddPostScreen(),
-          ),
-        ),
+        onPressed: () {
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
         tooltip: 'Add Post',
         child: const Icon(
           Icons.add,
